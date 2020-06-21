2a3c469aab4c216ed5fe88e9aa0c06450518988d
Oleksandr Orlov <luximetr@gmail.com>
2020-06-21 18:23:03 +0800

    Added react-navigation with stack.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,53 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-export default function App() {
+
+function Screen1() {
   return (
-    <View style={styles.container}>
-      <Text>TEXT</Text>
-    </View>
-  );
+     <View>
+       <Text>Screen1</Text>
+     </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Screen2() {
+  return (
+     <View>
+       <Text>Screen2</Text>
+     </View>
+  )
+}
+
+export const Flow1 = createStackNavigator(
+   {
+      Screen1: Screen1,
+      Screen2: Screen2
+   },
+   {
+      initialRouteName: 'Screen1'
+   }
+)
+
+const stack = createSwitchNavigator(
+   {
+      Flow1: {screen: Flow1}
+   },
+   {
+      initialRouteName: 'Flow1'
+   }
+)
+
+const AppContainer = createAppContainer(stack);
+
+export default class App extends React.Component {
+   render() {
+      return (
+         <View style={{flex: 1}}>
+            <AppContainer/>
+         </View>
+      )
+   }
+}
