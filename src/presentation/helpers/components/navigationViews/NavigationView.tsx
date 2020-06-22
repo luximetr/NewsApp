2772a8e34e7bad640d36aef87bad88c82707eac7
Oleksandr Orlov <luximetr@gmail.com>
2020-06-22 15:10:48 +0800

    Working on VectorIcons.

--- src/presentation/helpers/components/navigationViews/NavigationView.tsx
+++ src/presentation/helpers/components/navigationViews/NavigationView.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { styles } from './NavigationView.styles';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { VectorIcon } from '../imageViews/icons/VectorIcon';
 
 export class NavigationView extends React.Component {
 
@@ -17,6 +18,7 @@ export class NavigationView extends React.Component {
   private renderLeftAction() {
     return (
       <TouchableOpacity>
+        <VectorIcon name={'add'} source={'material'} size={30}/>
         <Text>Left</Text>
       </TouchableOpacity>
     )
