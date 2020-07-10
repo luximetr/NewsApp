5944f86f030a27cef9c17a727f0c53ffc0e03893
Oleksandr Orlov <luximetr@gmail.com>
2020-07-10 23:33:44 +0800

    Working on BottomAlert.

---
+++ src/presentation/helpers/components/alerts/bottomAlert/BottomAlert.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View} from "react-native";
+
+export class BottomAlert extends React.Component {
+
+   render() {
+      return (
+         <View />
+      )
+   }
+}
