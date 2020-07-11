ed9dc0031574bce02ed3cc305cd281f70621a126
Oleksandr Orlov <luximetr@gmail.com>
2020-07-11 13:08:50 +0800

    Added supported categories.

---
+++ src/model/model/category/Categories.tsx
@@ -0,0 +1,36 @@
+import {Category} from "./Category";
+
+const business = {
+   code: 'business',
+   name: 'Business'
+} as Category
+
+const entertainment = {
+   code: 'entertainment',
+   name: 'Entertainment'
+} as Category
+
+const general = {
+   code: 'general',
+   name: 'General'
+} as Category
+
+const health = {
+   code: 'health',
+   name: 'Health'
+} as Category
+
+const science = {
+   code: 'science',
+   name: 'Science'
+} as Category
+
+const sports = {
+   code: 'sports',
+   name: 'Sports'
+} as Category
+
+const technology = {
+   code: 'technology',
+   name: 'Technology'
+} as Category
