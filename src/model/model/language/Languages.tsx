0244b97bafba180d57535fa4a9096153dd2153d2
Oleksandr Orlov <luximetr@gmail.com>
2020-07-11 13:03:49 +0800

    Added supported Languages.

---
+++ src/model/model/language/Languages.tsx
@@ -0,0 +1,66 @@
+import {Language} from "./Language";
+
+const Arabic = {
+   code: 'ar',
+   name: 'Arabic'
+} as Language
+
+const German = {
+   code: 'de',
+   name: 'German'
+} as Language
+
+const English = {
+   code: 'en',
+   name: 'English'
+} as Language
+
+const Spanish = {
+   code: 'es',
+   name: 'Spanish'
+} as Language
+
+const French = {
+   code: 'fr',
+   name: 'French'
+} as Language
+
+const Hebrew = {
+   code: 'he',
+   name: 'Hebrew'
+} as Language
+
+const Italian = {
+   code: 'it',
+   name: 'Italian'
+} as Language
+
+const Dutch = {
+   code: 'nl',
+   name: 'Dutch'
+} as Language
+
+const Norwegian = {
+   code: 'no',
+   name: 'Norwegian'
+} as Language
+
+const Portuguese = {
+   code: 'pt',
+   name: 'Portuguese'
+} as Language
+
+const Russian = {
+   code: 'ru',
+   name: 'Russian'
+} as Language
+
+const NorthernSami = {
+   code: 'se',
+   name: 'Northern Sami'
+} as Language
+
+const Chinese = {
+   code: 'zh',
+   name: 'Chinese'
+} as Language
