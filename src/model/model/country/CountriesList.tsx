710cebfa5386e06ddc00a9d36cbe82cc9fd65884
Oleksandr Orlov <luximetr@gmail.com>
2020-07-11 12:44:54 +0800

    Added supported countries.

--- src/model/model/country/CountriesList.tsx
+++ src/model/model/country/CountriesList.tsx
@@ -60,9 +60,9 @@ const Cuba = {
    name: 'Cuba'
 } as Country
 
-const Czechia = {
+const CzechRepublic = {
    code: 'cz',
-   name: 'Czechia'
+   name: 'Czech Republic'
 } as Country
 
 const Germany = {
@@ -108,4 +108,164 @@ const Indonesia = {
 const Ireland = {
    code: 'ie',
    name: 'Ireland'
+} as Country
+
+const Israel = {
+   code: 'il',
+   name: 'Israel'
+} as Country
+
+const India = {
+   code: 'in',
+   name: 'India'
+} as Country
+
+const Italy = {
+   code: 'it',
+   name: 'Italy'
+} as Country
+
+const Japan = {
+   code: 'jp',
+   name: 'Japan'
+} as Country
+
+const SouthKorea = {
+   code: 'kr',
+   name: 'South Korea'
+} as Country
+
+const Lithuania = {
+   code: 'lt',
+   name: 'Lithuania'
+} as Country
+
+const Latvia = {
+   code: 'lv',
+   name: 'Latvia'
+} as Country
+
+const Morocco = {
+   code: 'ma',
+   name: 'Morocco'
+} as Country
+
+const Mexico = {
+   code: 'mx',
+   name: 'Mexico'
+} as Country
+
+const Malaysia = {
+   code: 'my',
+   name: 'Malaysia'
+} as Country
+
+const Nigeria = {
+   code: 'ng',
+   name: 'Nigeria'
+} as Country
+
+const Netherlands = {
+   code: 'nl',
+   name: 'Netherlands'
+} as Country
+
+const Norway = {
+   code: 'no',
+   name: 'Norway'
+} as Country
+
+const NewZealand = {
+   code: 'nz',
+   name: 'New Zealand'
+} as Country
+
+const Philippines = {
+   code: 'ph',
+   name: 'Philippines'
+} as Country
+
+const Poland = {
+   code: 'pl',
+   name: 'Poland'
+} as Country
+
+const Portugal = {
+   code: 'pt',
+   name: 'Portugal'
+} as Country
+
+const Romania = {
+   code: 'ro',
+   name: 'Romania'
+} as Country
+
+const Serbia = {
+   code: 'rs',
+   name: 'Serbia'
+} as Country
+
+const Russia = {
+   code: 'ru',
+   name: 'Russia'
+} as Country
+
+const SaudiArabia = {
+   code: 'sa',
+   name: 'Saudi Arabia'
+} as Country
+
+const Sweden = {
+   code: 'se',
+   name: 'Sweden'
+} as Country
+
+const Singapore = {
+   code: 'sg',
+   name: 'Singapore'
+} as Country
+
+const Slovenia = {
+   code: 'si',
+   name: 'Slovenia'
+} as Country
+
+const Slovakia = {
+   code: 'sk',
+   name: 'Slovakia'
+} as Country
+
+const Thailand = {
+   code: 'th',
+   name: 'Thailand'
+} as Country
+
+const Turkey = {
+   code: 'tr',
+   name: 'Turkey'
+} as Country
+
+const Taiwan = {
+   code: 'tw',
+   name: 'Taiwan'
+} as Country
+
+const Ukraine = {
+   code: 'ua',
+   name: 'Ukraine'
+} as Country
+
+const UnitedStates = {
+   code: 'us',
+   name: 'United States'
+} as Country
+
+const Venezuela = {
+   code: 've',
+   name: 'Venezuela'
+} as Country
+
+const SouthAfrica = {
+   code: 'za',
+   name: 'South Africa'
 } as Country
